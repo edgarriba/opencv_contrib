96964fee1a338e2eb739b8dfb5612929617d3a05
edgarriba <edgar.riba@gmail.com>
2016-04-10 12:40:28 +0200

    fix typo issues

--- modules/xfeatures2d/src/bold.cpp
+++ modules/xfeatures2d/src/bold.cpp
@@ -1,8 +1,6 @@
 #include "precomp.hpp"
 #include <fstream>
 
-
-
 // constants
 #define NTESTS 768
 #define DIMS 512
@@ -61,49 +59,48 @@ private:
 // constructor
 /* load tests and init 2 rotations
    for fast affine aprox. (example -20,20) */
-BOLD_Impl::BOLD_Impl(void)
-{
-  bin_tests = (int**) malloc(NROTS * sizeof(int *));
-  for (int i = 0; i < NROTS; i++){
-    bin_tests[i] = (int*)malloc(NTESTS*2 * sizeof(int));
-  }
-  std::ifstream file;
-  file.open("bold.descr");
-  /* read original tests and set them to rotation 0 */
-  for(int j = 0; j < NTESTS*2; j++ )
-    {
-      file >> bin_tests[0][j];
+BOLD_Impl::BOLD_Impl(void) {
+    bin_tests = (int**) malloc(NROTS * sizeof(int *));
+    for (int i = 0; i < NROTS; i++) {
+        bin_tests[i] = (int*)malloc(NTESTS*2 * sizeof(int));
     }
-  file.close();
-  rotations[0] = 20;
-  rotations[1] = -20;
-  /* compute the rotations offline */
-  for (int i = 0; i < NTESTS*2; i+=2) {
-    int x1 = bin_tests[0][i] % 32;
-    int y1 = bin_tests[0][i] / 32;
-    int x2 = bin_tests[0][i+1] % 32;
-    int y2 = bin_tests[0][i+1] / 32;
-    for (int a = 1; a < NROTS; a++) {
-      float angdeg = rotations[a-1];
-      float angle = angdeg*(float)(CV_PI/180.f);
-      float ca = (float)cos(angle);
-      float sa = (float)sin(angle);
-      int rotx1 = (x1-15)*ca - (y1-15)*sa + 16;
-      int roty1 = (x1-15)*sa + (y1-15)*ca + 16;
-      int rotx2 = (x2-15)*ca - (y2-15)*sa + 16;
-      int roty2 = (x2-15)*sa + (y2-15)*ca + 16;
-      bin_tests[a][i] = rotx1 + 32*roty1;
-      bin_tests[a][i+1] = rotx2 + 32*roty2;
+    // TODO: check if this can be hardcoded
+    std::ifstream file;
+    file.open("bold.descr");
+    /* read original tests and set them to rotation 0 */
+    for (int j = 0; j < NTESTS*2; j++) {
+        file >> bin_tests[0][j];
     }
-  }
+    file.close();
+    rotations[0] = 20;
+    rotations[1] = -20;
+    /* compute the rotations offline */
+    for (int i = 0; i < NTESTS*2; i+=2) {
+        int x1 = bin_tests[0][i] % 32;
+        int y1 = bin_tests[0][i] / 32;
+        int x2 = bin_tests[0][i+1] % 32;
+        int y2 = bin_tests[0][i+1] / 32;
+        for (int a = 1; a < NROTS; a++) {
+            float angdeg = rotations[a-1];
+            float angle = angdeg*(float)(CV_PI/180.f);
+            float ca = (float)cos(angle);
+            float sa = (float)sin(angle);
+            int rotx1 = (x1-15)*ca - (y1-15)*sa + 16;
+            int roty1 = (x1-15)*sa + (y1-15)*ca + 16;
+            int rotx2 = (x2-15)*ca - (y2-15)*sa + 16;
+            int roty2 = (x2-15)*sa + (y2-15)*ca + 16;
+            bin_tests[a][i] = rotx1 + 32*roty1;
+            bin_tests[a][i+1] = rotx2 + 32*roty2;
+        }
+    }
 }
 
 // destructor
 BOLD_Impl::~BOLD_Impl(void)
 {
   /* free the tests */
-  for (int i = 0; i < NROTS; i++){
-    free(bin_tests[i]);
+  for (int i = 0; i < NROTS; i++) {
+      free(bin_tests[i]);
   }
   free(bin_tests);
 }
@@ -134,13 +131,13 @@ void BOLD_Impl::compute(InputArray _image,
 
   Mat tmp_patch, desc, mask;
   for (int i = 0; i < num_kpts; ++i) {
-    // extract patch
-    // TODO: check patch size, by now set to 1
-    rectifyPatch(img, keypoints[i], 1, tmp_patch);
-    // compute descriptor
-    desc = descriptors.row(i);
-    mask = masks.row(i);
-    compute_patch(tmp_patch, desc, mask);
+      // extract patch
+      // TODO: check patch size, by now set to 1
+      rectifyPatch(img, keypoints[i], 1, tmp_patch);
+      // compute descriptor
+      desc = descriptors.row(i);
+      mask = masks.row(i);
+      compute_patch(tmp_patch, desc, mask);
   }
 }
 
@@ -216,34 +213,32 @@ inline void BOLD_Impl::compute_patch(const Mat& patch, Mat& descr, Mat& masks) {
 }
 
 /* masked distance  */
-inline int BOLD_Impl::hampopmaskedLR(uchar *a,uchar *ma,uchar *b,uchar *mb)
-{
-  int distL = 0;
-  int distR = 0;
-  int nL = 0;
-  int nR = 0;
-  for (int i = 0; i < 64; i++) {
-    int axorb = a[i] ^ b[i];
-    int xormaskedL = axorb & ma[i]  ;
-    int xormaskedR = axorb & mb[i]  ;
-    nL += ma[i];
-    nR += mb[i];
-    distL += __builtin_popcount(xormaskedL);
-    distR += __builtin_popcount(xormaskedR);
-  }
-  float n = nL + nR;
-  float wL = nL / n;
-  float wR = nR / n;
-  return distL*wL + distR*wR;
+inline int BOLD_Impl::hampopmaskedLR(uchar *a,uchar *ma,uchar *b,uchar *mb) {
+    int distL = 0;
+    int distR = 0;
+    int nL = 0;
+    int nR = 0;
+    for (int i = 0; i < 64; i++) {
+        int axorb = a[i] ^ b[i];
+        int xormaskedL = axorb & ma[i]  ;
+        int xormaskedR = axorb & mb[i]  ;
+        nL += ma[i];
+        nR += mb[i];
+        distL += __builtin_popcount(xormaskedL);
+        distR += __builtin_popcount(xormaskedR);
+    }
+    float n = nL + nR;
+    float wL = nL / n;
+    float wR = nR / n;
+    return distL*wL + distR*wR;
 }
 
 /* hamming distance  */
-inline int BOLD_Impl::hampop(uchar *a,uchar *b)
-{
+inline int BOLD_Impl::hampop(uchar *a,uchar *b) {
   int distL = 0;
   for (int i = 0; i < 64; i++) {
-    int axorb = a[i] ^ b[i];
-    distL += __builtin_popcount(axorb);
+      int axorb = a[i] ^ b[i];
+      distL += __builtin_popcount(axorb);
   }
   return distL;
 }
