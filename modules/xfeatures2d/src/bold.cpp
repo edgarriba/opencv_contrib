7317a6a2b11d5b54b0832ea0cafea9ef3cc352d6
edgarriba <edgar.riba@gmail.com>
2016-04-10 12:28:00 +0200

    final compute patch

--- modules/xfeatures2d/src/bold.cpp
+++ modules/xfeatures2d/src/bold.cpp
@@ -1,6 +1,8 @@
 #include "precomp.hpp"
 #include <fstream>
 
+
+
 // constants
 #define NTESTS 768
 #define DIMS 512
@@ -38,7 +40,12 @@ private:
      * BOLD functions
      */
 
-    inline void compute_patch(cv::Mat img, cv::Mat& descr,cv::Mat& masks);
+    inline void rectifyPatch(const Mat& image, const KeyPoint& kp,
+                             const int& patchSize, Mat& patch);
+
+    inline void compute_patch(const Mat& img,
+                              Mat& descr, Mat& masks);
+
     inline int hampopmaskedLR(uchar *a,uchar *ma,uchar *b,uchar *mb);
     inline int hampop(uchar *a,uchar *b);
 
@@ -105,27 +112,72 @@ BOLD_Impl::~BOLD_Impl(void)
 /* BOLD interface implementation */
 
 // keypoint scope
-void BOLD_Impl::compute( InputArray _image, std::vector<KeyPoint>& keypoints, OutputArray _descriptors )
-{
+void BOLD_Impl::compute(InputArray _image,
+                        std::vector<KeyPoint>& keypoints,
+                        OutputArray _descriptors) {
+  // do nothing if no image
+  const Mat img = _image.getMat();
+  if (img.empty()) return;
+
+  // allocate array
+  const int m_descriptor_size = static_cast<int>(DIMS);
+  const int num_kpts = static_cast<int>(keypoints.size());
+  _descriptors.create(num_kpts, m_descriptor_size, CV_8U);
+
+  // prepare descriptors
+  Mat descriptors = _descriptors.getMat();
+  descriptors.setTo(Scalar(0));
+
+  // prepare masks
+  // TODO: let's see how to interface that
+  Mat masks = descriptors.clone();
+
+  Mat tmp_patch, desc, mask;
+  for (int i = 0; i < num_kpts; ++i) {
+    // extract patch
+    // TODO: check patch size, by now set to 1
+    rectifyPatch(img, keypoints[i], 1, tmp_patch);
+    // compute descriptor
+    desc = descriptors.row(i);
+    mask = masks.row(i);
+    compute_patch(tmp_patch, desc, mask);
+  }
 }
 
 // -------------------------------------------------
 /* BOLD computation routines */
 
-inline void BOLD_Impl::compute_patch(cv::Mat img, cv::Mat& descr,cv::Mat& masks)
-{
+inline void BOLD_Impl::rectifyPatch(const Mat& image,
+                                    const KeyPoint& kp,
+                                    const int& patchSize,
+                                    Mat& patch) {
+    float s = 1.5f * (float) kp.size / (float) patchSize;
+
+    float cosine = (kp.angle>=0) ? cos(kp.angle*M_PI/180) : 1.f;
+    float sine   = (kp.angle>=0) ? sin(kp.angle*M_PI/180) : 0.f;
+
+    float M_[] = {
+        s*cosine, -s*sine,   (-s*cosine + s*sine  ) * patchSize/2.0f + kp.pt.x,
+        s*sine,   s*cosine,  (-s*sine   - s*cosine) * patchSize/2.0f + kp.pt.y
+    };
+
+    warpAffine(image, patch, Mat(2,3,CV_32FC1,M_), Size(patchSize, patchSize),
+              WARP_INVERSE_MAP + INTER_CUBIC + WARP_FILL_OUTLIERS);
+}
+
+inline void BOLD_Impl::compute_patch(const Mat& patch, Mat& descr, Mat& masks) {
   /* init cv mats */
-  int nkeypoints = 1;
+  /*int nkeypoints = 1;
   descr.create(nkeypoints, DIMS/8, CV_8U);
-  masks.create(nkeypoints, DIMS/8, CV_8U);
+  masks.create(nkeypoints, DIMS/8, CV_8U);*/
 
   /* apply box filter */
-  cv::Mat patch;
+  /*cv::Mat patch;
   boxFilter(img, patch, img.depth(), cv::Size(5,5),
-	    cv::Point(-1,-1), true, cv::BORDER_REFLECT);
+	    cv::Point(-1,-1), true, cv::BORDER_REFLECT);*/
 
   /* get test and mask results  */
-  int k =0;
+  int k = 0;
   uchar* dsc = descr.ptr<uchar>(k);
   uchar* msk = masks.ptr<uchar>(k);
   uchar *smoothed = patch.data;
@@ -135,34 +187,30 @@ inline void BOLD_Impl::compute_patch(cv::Mat img, cv::Mat& descr,cv::Mat& masks)
   unsigned int val = 0;
   unsigned int var = 0;
   int idx = 0;
-  int j=0;
+  int j = 0;
   int bit;
   int tdes,tvar;
-  for (int i = 0; i < DIMS; i++,j+=2){
-    bit = i%8;
-    int temp_var=0;
-    tdes = (smoothed[tests[j]] < smoothed[tests[j+1]]);
-    temp_var += (smoothed[r0[j]] < smoothed[r0[j+1]])^tdes;
-    temp_var += (smoothed[r1[j]] < smoothed[r1[j+1]])^tdes;
-    /* tvar-> 0 not stable --------  tvar-> 1 stable */
-    tvar = (temp_var == 0) ;
-    if (bit==0)
-      {
-	val = tdes;
-	var = tvar;
+  for (int i = 0; i < DIMS; i++, j+=2) {
+      bit = i % 8;
+      int temp_var = 0;
+      tdes = (smoothed[tests[j]] < smoothed[tests[j+1]]);
+      temp_var += (smoothed[r0[j]] < smoothed[r0[j+1]])^tdes;
+      temp_var += (smoothed[r1[j]] < smoothed[r1[j+1]])^tdes;
+      /* tvar-> 0 not stable --------  tvar-> 1 stable */
+      tvar = (temp_var == 0);
+      if (bit==0) {
+          val = tdes;
+          var = tvar;
+      } else {
+          val |= tdes << bit;
+          var |= tvar << bit;
       }
-    else
-      {
-	val |= tdes << bit;
-	var |= tvar << bit;
-      }
-    if (bit==7)
-      {
-	dsc[idx] = val;
-	msk[idx] = var;
-	val = 0;
-	var = 0;
-	idx++;
+      if (bit==7) {
+          dsc[idx] = val;
+          msk[idx] = var;
+          val = 0;
+          var = 0;
+          idx++;
       }
   }
 }
